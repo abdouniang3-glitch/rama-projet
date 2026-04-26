b86e4e56378094d990cafff8b82e0dc3cb2ea8dc
abdouniang3-glitch <abdou.niang3@univ-thies.sn>
2026-04-26 11:58:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-# Créer un main.py intelligent
-with open("main.py", "w") as f:
-    f.write("""
 import os
 
 vue = os.environ.get("VUE", "management")
 
 if vue == "agent":
-    with open("rama_agent (3) (1).py") as f2:
-        exec(f2.read().split("if __name__")[0], globals())
+    with open("rama_agent (3) (1).py") as f:
+        code = f.read().split("if __name__")[0]
 elif vue == "responsable":
-    with open("rama_responsable (3).py") as f2:
-        exec(f2.read().split("if __name__")[0], globals())
+    with open("rama_responsable (3).py") as f:
+        code = f.read().split("if __name__")[0]
 else:
-    with open("rama_management (2).py") as f2:
-        exec(f2.read().split("if __name__")[0], globals())
+    with open("rama_management (2).py") as f:
+        code = f.read().split("if __name__")[0]
 
+exec(code, globals())
 init_db()
-""")
-print("✅ main.py créé")
+
+# Rendre app accessible à gunicorn
+application = app
